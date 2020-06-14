eac0ba5ad111b3a50bd0c53da5980759621fabc2
mfurkanyuceal <furkanyuceal58@gmail.com>
2020-06-14 14:05:27 +0300

    second commit

---
+++ Controllers/SwapController.cs
@@ -0,0 +1,97 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SeniorWebAPIProjectV3.Models;
+using SeniorWebAPIProjectV3.Services;
+
+namespace SeniorWebAPIProjectV3.Controllers
+{
+    public class SwapController : ControllerBase
+    {
+        private readonly ILogger<SwapController> _logger;
+        private readonly ISwapService _swapService;
+
+        public SwapController(ILogger<SwapController> logger, ISwapService swapService)
+        {
+            _logger = logger;
+            _swapService = swapService;
+        }
+
+
+        [HttpGet]
+        public async Task<List<Swap>> GetAllSwaps()
+        {
+            return await _swapService.GetSwapListAsync();
+        }
+
+        [HttpGet]
+        public async Task<List<Swap>> GetSwapsByUserId([FromRoute] string userId)
+        {
+            return await _swapService.SearchSwapByUserIdAsync(userId);
+        }
+
+        [HttpGet]
+        public async Task<Swap> GetSwap([FromRoute] string swapId)
+        {
+            return await _swapService.GetSwapBySwapIdAsync(swapId);
+        }
+
+        [HttpGet]
+        public async Task<bool> Delete([FromRoute] string swapId)
+        {
+            var swap = await _swapService.GetSwapBySwapIdAsync(swapId);
+
+            if (swap == null)
+            {
+                return false;
+            }
+
+
+            var deleted = await _swapService.DeleteSwapBySwapIdAsync(swapId);
+
+            if (deleted)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        [HttpPost]
+        public async Task<Swap> Update([FromBody] Swap swap)
+        {
+            var tempSwap = await _swapService.GetSwapBySwapIdAsync(swap.swapID);
+
+            if (tempSwap == null)
+            {
+                return null;
+            }
+
+
+            var updatedSwap = await _swapService.UpdateSwapAsync(swap);
+
+
+            if (updatedSwap)
+            {
+                return await _swapService.GetSwapBySwapIdAsync(swap.swapID);
+            }
+
+            return null;
+        }
+
+
+        [HttpPost]
+        public async Task<Swap> Create([FromBody] Swap swap)
+        {
+            var created = await _swapService.AddSwapAsync(swap);
+
+            if (!created)
+            {
+                return null;
+            }
+
+            return await _swapService.GetSwapBySwapIdAsync(swap.swapID);
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SeniorWebAPIProjectV3.Models;
+using SeniorWebAPIProjectV3.Services;
+
+namespace SeniorWebAPIProjectV3.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly IUserService _userService;
+
+        public UserController(ILogger<UserController> logger, IUserService userService)
+        {
+            _logger = logger;
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<List<User>> GetUsers()
+        {
+            return await _userService.GetUserListAsync();
+        }
+
+        [HttpGet]
+        public async Task<User> GetUser([FromRoute] string userId)
+        {
+            return await _userService.GetUserByUserIdAsync(userId);
+        }
+        
+        [HttpGet]
+        public async Task<User> GetUserByUserName([FromRoute] string userName)
+        {
+            return await _userService.GetUserByUserNameAsync(userName);
+        }
+        
+        [HttpGet]
+        public async Task<List<User>> GetUsersByFancy([FromRoute] string fancyName)
+        {
+            return await _userService.SearchUserByFancyNameAsync(fancyName);
+        }
+        
+        [HttpGet]
+        public async Task<List<User>> GetUsersByAbility([FromRoute] string abilityName)
+        {
+            return await _userService.SearchUserByAbilityNameAsync(abilityName);
+        }
+
+        [HttpPut]
+        public async Task<User> Update([FromBody] User user)
+        {
+            var updated = await _userService.UpdateUserAsync(user);
+
+            if (updated)
+            {
+                return await _userService.GetUserByUserIdAsync(user.userID);
+            }
+
+            return null;
+        }
+
+        [HttpDelete]
+        public async Task<bool> Delete([FromRoute] string userId)
+        {
+            var deleted = await _userService.DeleteUserByUserIdAsync(userId);
+
+            return deleted;
+        }
+
+
+        [HttpPost]
+        public async Task<User> Create([FromBody] User user)
+        {
+            var created = await _userService.AddUserAsync(user);
+
+            if (!created)
+            {
+                return null;
+            }
+
+            return await _userService.GetUserByUserIdAsync(user.userID);
+        }
+    }
+}
